7fe0560eb310686fbcf57a8cde8092ad48adcceb
Yashika Geed <yashika4824.be23@chitkara.edu.in>
2025-09-16 11:49:44 +0530

    Add files via upload

---
+++ FavoriteBooks/FavoriteBooks/BookFormTableViewController.swift
@@ -0,0 +1,118 @@
+//
+//  BookFormTableViewController.swift
+//  FavoriteBooks
+//
+//  Created by Yashika Geed on 16/09/25.
+//
+
+import UIKit
+
+class BookFormTableViewController: UITableViewController {
+
+    var book: Book?
+    
+    @IBOutlet weak var Titletextfield: UITextField!
+    @IBOutlet weak var Authortextfield: UITextField!
+    @IBOutlet weak var Genretextfield: UITextField!
+    @IBOutlet weak var Lengthtextfield: UITextField!
+    init?(coder: NSCoder, book: Book?) {
+        self.book = book
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.book = nil
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateView()
+    }
+    
+    func updateView() {
+        guard let book = book else {return}
+        
+        Titletextfield.text = book.title
+        Authortextfield.text = book.author
+        Genretextfield.text = book.genre
+        Lengthtextfield.text = book.length
+    }
+    
+    @IBAction func SaveButton(_ sender: Any) {
+        guard let title = Titletextfield.text,
+            let author = Authortextfield.text,
+            let genre = Genretextfield.text,
+            let length = Lengthtextfield.text else {return}
+        
+        book = Book(title: title, author: author, genre: genre, length: length)
+        performSegue(withIdentifier: "UnwindToBookTable", sender: self)
+    }
+    
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
